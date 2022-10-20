7b5965a98ee7a82c195c6c98cd5ecf1c0f427ac4
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2022-10-20 20:53:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.simondice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    val colores = arrayOf("Verde", "Rojo", "Amarillo", "Azul")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bInicio : Button = findViewById(R.id.inicioB)
+        bInicio.setOnClickListener{
+            empezar(bInicio)
+        }
+    }
+
+    fun elec(): String {
+
+        val colRan = when(Random.nextInt(4) + 1){
+            1 -> colores[0]
+            2 -> colores[1]
+            3 -> colores[2]
+            else -> colores[3]
+        }
+
+        return colRan
+    }
+
+    fun empezar(view : View){
+        val col = elec()
+        val emp = Intent(this, col::class.java)
+        startActivity(emp)
+    }
+}
